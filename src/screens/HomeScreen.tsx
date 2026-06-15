24376ad7727c9f76187e3a413e26b8adf2a09c77
prachirajpurohit <248518054+prachirajpurohit@users.noreply.github.com>
2026-06-16 00:02:01 +0530

    homescreen base

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { getPopularMovies } from '../services/TMDB';
 
 export default function HomeScreen() {
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState<any[]>([]);
+    const [errMessage, setErrMessage] = useState('');
+
+    useEffect(() => {
+        const movieData = async () => {
+            try {
+                setData(await getPopularMovies());
+            } catch (error) {
+                setErrMessage(error instanceof Error ? error.message : String(error));
+            }
+            setLoading(false);
+        };
+        movieData();
+    }, []);
+
+    if (loading) return <Text>Loading...</Text>;
+    if (errMessage) return <Text>{errMessage}</Text>;
+
     return (
         <View>
-            <Text>HomeScreen</Text>
+            <FlatList
+                data={data}
+                renderItem={({ item }) => <Text>{item.title}</Text>}
+                keyExtractor={item => item.id.toString()}
+            />
         </View>
-    )
+    );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
